dacd64db04482c98720266234c0b42bbc4ad1a7c
Abdullah Tariq <abdullahtariq977961@gmail.com>
2026-01-13 00:27:04 +0500

    add sign-up and sign-in page views and paths

--- DotCoder_pages/urls.py
+++ DotCoder_pages/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('sign-up/', views.sign_up_page, name='sign_up_page'),
+    path('sign-in/', views.sign_in_page, name='sign_in_page'),
 ]
--- DotCoder_pages/views.py
+++ DotCoder_pages/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def landing_page(request):
     return render(request, 'landing_page.html')
 
+def sign_up_page(request):
+    return render(request, 'sign_up_page.html')
+
+def sign_in_page(request):
+    return render(request, 'sign_in_page.html')
